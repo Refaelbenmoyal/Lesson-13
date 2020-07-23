70ebdcca102e0299f87b4fd9e8bd9e1f438e29a8
refaelbenmoyal <67203489+Refaelbenmoyal@users.noreply.github.com>
2020-07-23 08:31:38 +0300

    Lesson 13

---
+++ Computer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    [DebuggerDisplay("{price * 3.67f}")]
+    class Computer
+    {
+        //Data
+        public string model;
+        public int price;
+        public int numberOfProcessors;
+        public float screenSize;
+        public bool isTurnOn;
+
+        //functionallity
+         public void TellMeThePrice()
+        {
+            Console.WriteLine($"{ model} price is {price }");
+        }
+         public float TellMeTheScreenSize()
+        {
+            return screenSize;
+        }
+        public void TurnOn()
+        {
+            if (isTurnOn)
+            {
+                Console.WriteLine($"{model} is already on");
+            }
+            else
+            {
+                isTurnOn = false;
+                Console.WriteLine($"turning on {model}...");
+            }
+        }
+        public void TurnOof()
+        {
+            if (!isTurnOn)
+            {
+                Console.WriteLine($"{model} is already oof");
+            }
+            else
+            {
+                isTurnOn = true;
+                Console.WriteLine($"turning oof {model}...");
+            }
+        }
+        public void AddProcessor(int processor)
+        {
+            numberOfProcessors = numberOfProcessors + processor;
+            Console.WriteLine($"{model} Has got {numberOfProcessors}");
+        }
+        public override string ToString()
+        {
+            return $"Computer [model]:{model} [price]:{price} [numberOfProcessors]:{numberOfProcessors} [screenSize]:{screenSize} [isTurnOn]:{isTurnOn}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myMac = new Computer()
+            {
+                model = "IOS 10",
+                price = 10000,
+                numberOfProcessors = 16,
+                screenSize = 28,
+                isTurnOn = true,
+            };
+            Computer myHp = new Computer()
+            {
+                model = "HP 8000",
+                price = 5000,
+                numberOfProcessors = 8,
+                screenSize = 24,
+                isTurnOn = true,
+            };
+            Computer myLenovo = new Computer()
+            {
+                model = "v14",
+                price = 4000,
+                numberOfProcessors = 16,
+                screenSize = 28,
+                isTurnOn = true,
+            };
+            myHp.TellMeThePrice();
+            myMac.TellMeTheScreenSize();
+            myHp.TurnOn();
+            myLenovo.TurnOof();
+            Console.WriteLine(myLenovo);
+            myLenovo.ToString();
+
+
+        }
+    }
+}
